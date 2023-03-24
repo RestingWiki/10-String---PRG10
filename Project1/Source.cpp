f2f3d855040c82f608e309e5e04e24460e66f399
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-03-24 21:14:38 +0700

    Completed

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -6,41 +6,69 @@ using namespace std;
 /////////////////////////
 // Function definition //
 /////////////////////////
-void replace(string& s1, string& s2, string& s3);
+bool checkPalindromes(const string& line);
+void reverse(string& s, string::size_type left, string::size_type right);
 
 
 int main() {
-
 	// User's input
-	string s1, s2, s3;
+	string s1, s2;
 
-	cout << "Initial string: ";
+	cout << "Input string: ";
 	getline(cin, s1);
 
-	cout << "String to be replaced: ";
-	getline(cin, s2);
+	if (checkPalindromes(s1))
+		cout << "This is a palindromes!" << endl;
+	else
+		cout << "This isn't a palindromes" << endl;
 
-	cout << "String to replace with: ";
-	getline(cin, s3);
-
-	replace(s1, s2, s3);
-
-	cout << "New string: " << s1 << endl;
 
 	return 0;
 }
 
-void replace(string& s1, string& s2, string& s3) {
-	string:: size_type pos = s1.find_first_of(s2);
 
+/////////////////////////
+// Function definition //
+/////////////////////////
+bool checkPalindromes(const string& line) {
+
+	// Create a copy of the string 
+	string s2 = line;
+	
+	// Convert to uppercase
+	for (int i = 0; i < s2.length(); i++)
+	{
+		if (97 <= s2.at(i) && s2.at(i) <= 122)
+			s2.at(i) -= 32;
+	}
+	
+	// Replace special letter, whitespace
+	string::size_type wStart, wEnd;
+	string delimeter = " :',";
+	int pos = s2.find_first_of(delimeter);
 
 	while (pos != string::npos)
 	{
-		s1.erase(pos,s2.length());
-		s1.insert(pos, s3);
-		pos = s1.find_first_of(s2);
+		s2.erase(pos,1);
+		pos = s2.find_first_of(delimeter);
 	}
 
+	string s3 = s2;
+	// Reverse the string then compare
+	reverse(s3,0, s3.length()-1);
 
-
+	return (s2 == s3);
+}
+void reverse(string& s, string:: size_type left, string::size_type right) {
+	if (left >= right)
+	{
+		return;
+	}
+	else
+	{
+		char temp = s.at(left);
+		s.at(left) = s.at(right);
+		s.at(right) = temp;
+		reverse(s,++left,--right);
+	}
 }
